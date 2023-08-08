6d22f51147f2f7fd45503d4ca2b9c205bdae292a
hoisn12 <87404872+hoisn12@users.noreply.github.com>
2023-08-08 01:06:23 +0000

    수정

--- src/components/TerminalComponent/index.tsx
+++ src/components/TerminalComponent/index.tsx
@@ -7,21 +7,73 @@ const TerminalComponent = () => {
 
     useEffect(() => {
         if(terminalRef?.current){
-        const terminal = new Terminal();
-        terminal.open(terminalRef.current);
+        const term = new Terminal();
+        term.open(terminalRef.current);
 
-        terminal.write('Hello from \x1B[1;3;31mshellow!\x1B[0m $ ')
+        term.write('Hello from \x1B[1;3;31mshellow!\x1B[0m $ ')
+        let cursor = 0;
+        let input = "";
 
 
-        terminal.onData((data) => {
-            console.log("data",data)
-            // 입력 데이터 처리 로직 추가
-            // 예시: 입력된 데이터를 서버로 전송 등
-            terminal.write(data);
-          });
+        // 입력 데이터 처리 로직 추가
+        // 예시: 입력된 데이터를 서버로 전송 등
+        term.onData(data => {            
+            let code = data.charCodeAt(0);
+            console.log("data", data)
+            console.log("code: ",code)
+
+            if(code == 13){
+              if(input === "clean"){
+                term.clear()
+                
+              }else{
+                term.writeln("")
+                term.write('Hello from \x1B[1;3;31mshellow!\x1B[0m $ ')
+              }
+              
+              
+              input = "";
+              cursor = 0;
+            }else if (code == 27) {
+              console.log("conde??", data.slice(1))
+              console.log("cursor: ",cursor)
+              console.log("cursor: ",input.length)
+              switch (data.slice(1)) {
+                case '[C': // Right arrow
+                  if (cursor < input.length) {
+                    cursor += 1;
+                    term.write(data);
+                    
+                  }
+                  break;
+                case '[D': // Left arrow
+                  if (cursor > 0) {
+                    cursor -= 1;
+                    term.write(data);
+                  }
+          
+                  break;
+              }
+            }else if (code < 32 || code == 127) { // Control
+              if(input.length > 0){
+                term.write('\b \b')
+                input = input.slice(0, input.length - 1)
+                console.log("input: ",input)
+              }
+              return;
+            }else{
+              console.log("slice", input.slice(0, cursor))
+              input = input.slice(0, cursor) + data + input.slice(cursor);
+              console.log("input", input)
+              cursor += 1
+              term.write(data)              
+            }
+            
+            
+        });
       
         return () => {
-            terminal.dispose();
+          term.dispose();
           };
         }
     },[])
